4d96dec742d737f74a689029df824027364f8035
zfireear <zfireear@gmail.com>
2016-06-13 21:23:14 +0800

    add weather icon

--- app/src/main/java/com/test/z/weather_android/MainActivity.java
+++ app/src/main/java/com/test/z/weather_android/MainActivity.java
@@ -1,7 +1,7 @@
 package com.test.z.weather_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("hello", "hi");
+
     }
 }
